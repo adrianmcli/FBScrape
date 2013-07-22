72e9a7b869ae8ef60092e80637b77c92bf3dfdf5
ibopm <li.adrianmc@gmail.com>
2013-07-22 04:35:00 -0400

    clean up comments

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-//var app = require('express').createServer();
+
 var express = require('express')
   , routes = require('./routes')
   , http = require('http')
@@ -6,10 +6,10 @@ var express = require('express')
 
 var app = express();
 
-// all environments
+// Configs for all environments 
 app.set('port', process.env.VCAP_APP_PORT || 3000);
-//app.set('views', __dirname + '/views');
-//app.set('view engine', 'jade');
+app.set('views', __dirname + '/views');
+app.set('view engine', 'jade');
 app.use(express.favicon());
 app.use(express.logger('dev'));
 app.use(express.bodyParser());
@@ -17,12 +17,12 @@ app.use(express.methodOverride());
 app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
 
-// development only
+// Configs for development only
 if ('development' == app.get('env')) {
   app.use(express.errorHandler());
 }
 
-// Process request
+// Route requests
 app.get('/', function(req, res) {
     res.send('<h1>Welcome to the Collaborative Problem Space</h1><p>The collaborative problem space is currently under construction, please stay tuned!</p><h2>Less Talk, More Do</h2><p>Rather than spending my time reading books, articles, and looking at "motivational porn", I decided to actually start something I feel passionate about.</p><h2>The Idea</h2><p>To give a space for developing ideas and solutions with a problem-focused approach.</p><h2>Practical Purpose for Us</h2><p>1) Learn Node.js back-end programming</p><p>2) Learn Angular.js front-end programming</p><p>3) Resume padder; interviewers are going to be impressed when they hear that you participated in a project which approaches idea generation in a novel way.</p>');
 });
@@ -31,6 +31,3 @@ app.get('/', function(req, res) {
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
 });
-
-//app.listen(process.env.VCAP_APP_PORT || 3000);
-//console.log('Listening on port 3000');
